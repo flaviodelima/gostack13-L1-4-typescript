204fa6cb576b4f6110e45246487c01efd872d588
Flavio de Lima <flaviolimpio@gmail.com>
2020-10-18 14:40:26 -0300

    aula 03 - Quando adicionar tipos

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express, { request, response } from 'express';
+import { helloWorld } from './routes'
 const app = express();
 
-app.get('/', (request, response)=>{
-    return response.json({ message: 'Hello World' });
-})
+app.get('/', helloWorld)
 
 app.listen(3333);
